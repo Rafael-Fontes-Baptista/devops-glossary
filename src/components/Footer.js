5cef02ff5a17660ee3cc5afb5ce94118c7c21f1d
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-05-15 22:03:11 +0200

    Add Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear()
+  return (
+    <div className="flex-container">
+      <div id="contact" class="contactSection">
+        <div className="text-center">
+          <p className="subHeading">Like the BMI app?</p>
+          <h2>I'd love to hear from you!</h2>
+          <a href="mailTo:rafaelfontesb@gmail.com" class="contactButton">
+            GET IN TOUCH!
+          </a>
+        </div>
+      </div>
+      <div className="footer_icons">
+        <div>
+          <a href="https://www.linkedin.com/in/rafael-fontes-baptista/">
+            <i className="fab fa-linkedin-in fa-2x"></i>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/Rafael-Fontes-Baptista">
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+        </div>
+      </div>
+      <div className="footer_text">
+        Copyright &copy; {currentYear} | Rafael Baptista
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./components.css"
+
+export default function Header() {
+  return (
+    <div>
+      <div className="header">
+        <h1>Devops Glossary</h1>
+      </div>
+      <div className="sub-title">
+        <p>Improve your vocabulary in the development world!</p>
+      </div>
+    </div>
+  )
+}
